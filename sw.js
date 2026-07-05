2233d634bae2a4a72ea1dcdb57107afae372da88
shimihillel <shimrit.assaf@gmail.com>
2026-07-05 10:30:02 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'eifo-ze-v19-mockup-from-scratch';
+const CACHE_NAME = 'eifo-ze-v20-stable-mockup';
 const ASSETS = ['./','./index.html','./style.css','./app.js','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./favicon.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
